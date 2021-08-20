5b21c2e0d2a9ce98cc6d9578de7bfb55105b95a5
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-20 15:28:12 -0400

    Handle error exceptions
    Handle AuthenticationException, NotFoundHttpException, MethodNotAllowedHttpException, QueryException and unexpected Exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,9 +3,15 @@
 namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Exception\MethodNotAllowedException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -61,7 +67,49 @@ class Handler extends ExceptionHandler
             $modelo = strtolower(class_basename($exception->getModel()));
             return $this->errorResponse("No existe ninguna instancia de {$modelo} con el id especificado", 404);
         }
-        return parent::render($request, $exception);
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
+
+        if ($exception instanceof AuthenticationException) {
+            return $this->errorResponse('No posee permisos para ejecutar esta acción', 403);
+        }
+
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorResponse('No se encontró la url especificada', 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorResponse('EL método especificado en la petición no es válido', 405);
+        }
+
+        if ($exception instanceof HttpException) {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if ($exception instanceof QueryException) {
+            /*
+             * dd($exception);
+             * respuesta dd (retorna informacion de la db
+             * +errorInfo: array:3 [
+                0 => "23000"
+              1 => 1451
+              2 => "Cannot delete or update a parent row: a foreign key constraint fails (`homestead`.`transactions`,
+                    CONSTRAINT `transactions_buyer_id_foreign` FOREIGN KEY (`buyer_id`) REFERENCES `users` (`id`))"
+                ]
+            */
+            $codigo = $exception->errorInfo[1];
+            if ($codigo == 1451){
+                return $this->errorResponse('No se puede eliminar de forma permanente el recurso, por que está relacionado con algun otro', 409);
+            }
+
+        }
+        if (config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+        return $this->errorResponse('Fala inesperada. Intente luego', 500);
+
+
     }
 
 
