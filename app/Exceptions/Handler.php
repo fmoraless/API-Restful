79d58e80fc70d8d8541772bb311213c869d3e1d5
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-20 14:45:18 -0400

    Return validation errors in json format

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,15 @@
 
 namespace App\Exceptions;
 
+use App\Traits\ApiResponser;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Validation\ValidationException;
 use Throwable;
 
 class Handler extends ExceptionHandler
 {
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -50,6 +54,28 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+        if ($exception instanceof ValidationException){
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+        if ($exception instanceof ModelNotFoundException) {
+            $modelo = strtolower(class_basename($exception->getModel()));
+            return $this->errorResponse("No existe ninguna instancia de {$modelo} con el id especificado", 404);
+        }
         return parent::render($request, $exception);
+    }
+
+
+    /**
+     * Create a response object from the given validation exception.
+     *
+     * @param  \Illuminate\Validation\ValidationException  $e
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+    protected function convertValidationExceptionToResponse(ValidationException $e, $request)
+    {
+        $errors = $e->validator->errors()->getMessages();
+        return $this->errorResponse($errors, 422);
+
     }
 }
--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -118,19 +118,19 @@ class UserController extends ApiController
 
         if ($request->has('admin')) {
             if (!$user->esVerificado()) {
-                return response()->json([
-                    'error' => 'Sólo usuarios verificados pueden cambiar su valor a administrador',
-                    'code' => 409
-                ], 409);
+                return $this->errorResponse(
+                    'Sólo usuarios verificados pueden cambiar su valor a administrador',
+                     409
+                );
             }
             $user->admin = $request->admin;
         }
 
         if (!$user->isDirty()){
-            return response()->json([
-                'error' => 'Se debe especificar al menos un valor diferente para actualizar',
-                'code' => 422
-            ], 422);
+            return $this->errorResponse(
+                'Se debe especificar al menos un valor diferente para actualizar',
+                422
+            );
         }
 
         $user->save();
