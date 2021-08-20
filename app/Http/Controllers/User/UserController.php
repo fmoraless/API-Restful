3b69e47bca24f555d801598b65e794423ad269b1
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-20 17:58:50 -0400

    Implicit injection and global scopes

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -65,11 +65,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(User $user)
     {
-        $usuario = User::findOrFail($id);
-
-        return $this->showOne($usuario);
+        return $this->showOne($user);
     }
 
     /**
@@ -90,9 +88,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(Request $request, User $user)
     {
-        $user = User::findOrFail($id);
+        /*$user = User::findOrFail($id);*/
 
         $rules = [
             'email' => 'email|unique:users,email,' . $user->id,
@@ -144,9 +142,9 @@ class UserController extends ApiController
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(User $user)
     {
-        $user = User::findOrFail($id);
+        /*$user = User::findOrFail($id);*/
 
         $user->delete();
 
