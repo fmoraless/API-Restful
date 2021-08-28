6597a354fc3a7aa132542b7154204b17fded8cdf
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-28 15:54:08 -0400

    exclude pivot fields on the answer

--- app/Category.php
+++ app/Category.php
@@ -15,6 +15,31 @@ class Category extends Model
         'name', 'description'
     ];
 
+    // Borrar campos pivot de la respuesta
+/*
+"data": [
+    {
+        "id": 1,
+        "name": "est",
+        "description": "In distinctio quis placeat dolor labore aliquam suscipit.",
+        "quantity": 1,
+        "status": "no disponible",
+        "image": "1.jpg",
+        "seller_id": 34,
+        "deleted_at": null,
+        "created_at": "2021-08-20T22:22:30.000000Z",
+        "updated_at": "2021-08-20T22:22:30.000000Z",
+        "pivot": {
+        "category_id": 2,        ========> Como eliminar estos campos pivot
+        "product_id": 1          ========> Como eliminar estos campos pivot
+        }
+    }
+*/
+
+    protected $hidden = [
+      'pivot'
+    ];
+
     public function products()
     {
         return $this->belongsToMany(Product::class);
