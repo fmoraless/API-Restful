fd7c162c29b4d5a8c9742194edf357d2429ab58e
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-28 15:55:15 -0400

    load relation product.seller on BuyerSellerController.php

--- app/Http/Controllers/Buyer/BuyerSellerController.php
+++ app/Http/Controllers/Buyer/BuyerSellerController.php
@@ -16,7 +16,8 @@ class BuyerSellerController extends ApiController
      */
     public function index(Buyer $buyer)
     {
-        $sellers = $buyer->transactions()->with('product.seller')
+        $sellers = $buyer->transactions()
+            ->with('product.seller')
             ->get()
             ->pluck('product.seller')
             ->unique('id')
