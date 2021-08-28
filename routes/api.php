dbdff0b0f1b8955545e273466f0a649de4fa50ad
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-28 16:07:02 -0400

    update routes API

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,10 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 Route::resource('buyers','Buyer\BuyerController', ['only' => ['index', 'show']]);
+Route::resource('buyers.transactions','Buyer\BuyerTransactionController', ['only' => ['index']]);
+Route::resource('buyers.products','Buyer\BuyerProductController', ['only' => ['index']]);
+Route::resource('buyers.sellers','Buyer\BuyerSellerController', ['only' => ['index']]);
+Route::resource('buyers.categories','Buyer\BuyerCategoryController', ['only' => ['index']]);
 
 /*
 |--------------------------------------------------------------------------
@@ -32,6 +36,10 @@ Route::resource('buyers','Buyer\BuyerController', ['only' => ['index', 'show']])
 |--------------------------------------------------------------------------
 */
 Route::resource('categories','Category\CategoryController', ['except' => ['create', 'edit']]);
+Route::resource('categories.products','Category\CategoryProductController', ['only' => ['index']]);
+Route::resource('categories.sellers','Category\CategorySellerController', ['only' => ['index']]);
+Route::resource('categories.transactions','Category\CategoryTransactionController', ['only' => ['index']]);
+Route::resource('categories.buyers','Category\CategoryBuyerController', ['only' => ['index']]);
 
 /*
 |--------------------------------------------------------------------------
@@ -39,6 +47,10 @@ Route::resource('categories','Category\CategoryController', ['except' => ['creat
 |--------------------------------------------------------------------------
 */
 Route::resource('products','Product\ProductController', ['only' => ['index', 'show']]);
+Route::resource('products.transactions','Product\ProductTransactionController', ['only' => ['index']]);
+Route::resource('products.buyers','Product\ProductBuyerController', ['only' => ['index']]);
+Route::resource('products.buyers.transactions','Product\ProductBuyerTransactionController', ['only' => ['store']]);
+Route::resource('products.categories','Product\ProductCategoryController', ['only' => ['index', 'update', 'destroy']]);
 
 /*
 |--------------------------------------------------------------------------
@@ -46,6 +58,8 @@ Route::resource('products','Product\ProductController', ['only' => ['index', 'sh
 |--------------------------------------------------------------------------
 */
 Route::resource('transactions','Transaction\TransactionController', ['only' => ['index', 'show']]);
+Route::resource('transactions.categories','Transaction\TransactionCategoryController', ['only' => ['index']]);
+Route::resource('transactions.sellers','Transaction\TransactionSellerController', ['only' => ['index']]);
 
 /*
 |--------------------------------------------------------------------------
@@ -53,6 +67,10 @@ Route::resource('transactions','Transaction\TransactionController', ['only' => [
 |--------------------------------------------------------------------------
 */
 Route::resource('sellers','Seller\SellerController', ['only' => ['index', 'show']]);
+Route::resource('sellers.buyers','Seller\SellerBuyerController', ['only' => ['index']]);
+Route::resource('sellers.products','Seller\SellerProductController', ['except' => ['create', 'show', 'edit']]);
+Route::resource('sellers.transactions','Seller\SellerTransactionController', ['only' => ['index']]);
+Route::resource('sellers.categories','Seller\SellerCategoryController', ['only' => ['index']]);
 
 /*
 |--------------------------------------------------------------------------
