8e37593711f5a2e11bceac3a3b143587cfddc62c
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-09-14 01:59:22 -0300

    get inverse transformed attributes

--- app/Http/Middleware/TransformInput.php
+++ app/Http/Middleware/TransformInput.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Validation\ValidationException;
 
 class TransformInput
 {
@@ -16,9 +17,28 @@ class TransformInput
     public function handle($request, Closure $next, $transformer)
     {
         $transformedInput = [];
-        foreach ($request->request->all() as $i) {
-            
+        foreach ($request->request->all() as $input => $value) {
+            $transformedInput[$transformer::originalAttribute($input)] = $value;
         }
-        return $next($request);
+        $request->replace($transformedInput);
+
+        $response = $next($request);
+
+        if (isset($response->exception) && $response->exception instanceof ValidationException) {
+            $data = $response->getData();
+
+            $tranformedErrors = [];
+
+            foreach ($data->error as $field => $error) {
+                $transformedField = $transformer::transformedAttribute($field);
+                $tranformedErrors[$transformedField] = str_replace($field, $transformedField, $error);
+            }
+
+            $data->error = $tranformedErrors;
+
+            $response->setData($data);
+        }
+
+        return $response;
     }
 }
